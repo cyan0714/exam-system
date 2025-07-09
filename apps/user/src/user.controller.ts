0b444d1b7a935e599fc45c66f569ac4ed30f71cd
cyan0714 <917244295@qq.com>
2025-07-09 13:58:39 +0800

    feat: 用户注册

--- apps/user/src/user.controller.ts
+++ apps/user/src/user.controller.ts
@@ -1,17 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, Inject, Query } from '@nestjs/common';
 import { UserService } from './user.service';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { EmailService } from '@app/email';
 import { RedisService } from '@app/redis';
 
-@Controller()
+@Controller('user')
 export class UserController {
-  constructor(
-    private readonly userService: UserService,
-    private readonly redisService: RedisService,
-  ) {}
+  constructor(private readonly userService: UserService) {}
 
-  @Get()
-  async getHello(): Promise<string> {
-    const keys = await this.redisService.keys('*');
-    return this.userService.getHello() + keys;
+  @Inject(EmailService)
+  private emailService: EmailService;
+
+  @Inject(RedisService)
+  private redisService: RedisService;
+
+  @Get('register-captcha')
+  async captcha(@Query('address') address: string) {
+    const code = Math.random().toString().slice(2, 8);
+
+    await this.redisService.set(`captcha_${address}`, code, 5 * 60);
+
+    await this.emailService.sendMail({
+      to: address,
+      subject: '注册验证码',
+      html: `<p>你的注册验证码是 ${code}</p>`,
+    });
+    return '发送成功';
+  }
+
+  @Post('register')
+  async register(@Body() registerUser: RegisterUserDto) {
+    return await this.userService.register(registerUser);
   }
 }
