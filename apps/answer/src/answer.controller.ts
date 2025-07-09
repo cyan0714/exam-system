96d4331117cdcd24b8694d476c14c237fa5b5281
cyan0714 <917244295@qq.com>
2025-07-09 11:20:43 +0800

    feat: init(微服务、Lib拆分)

---
+++ apps/answer/src/answer.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Inject } from '@nestjs/common';
+import { AnswerService } from './answer.service';
+import { ClientProxy } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
+
+@Controller()
+export class AnswerController {
+  constructor(private readonly answerService: AnswerService) {}
+
+  @Inject('EXAM_SERVICE')
+  private readonly examClient: ClientProxy;
+
+  @Get()
+  async getHello(): Promise<string> {
+    const value = await firstValueFrom(this.examClient.send('sum', [1, 3, 5]));
+    console.log(value);
+    return this.answerService.getHello() + value;
+  }
+}
---
+++ apps/exam/src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { ExamModule } from './exam.module';
+import { Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const app = await NestFactory.create(ExamModule);
+  await app.listen(3002);
+
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      port: 8888,
+    },
+  });
+  app.startAllMicroservices();
+}
+bootstrap();
