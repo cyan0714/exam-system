87bee6a3d1e58546280a17be513da88411573439
cyan0714 <917244295@qq.com>
2025-07-09 14:57:43 +0800

    feat: 用户登录、修改密码

--- apps/user/src/user.module.ts
+++ apps/user/src/user.module.ts
@@ -4,10 +4,35 @@ import { UserService } from './user.service';
 import { RedisModule } from '@app/redis';
 import { PrismaModule } from '@app/prisma';
 import { EmailModule } from '@app/email';
+import { JwtModule } from '@nestjs/jwt';
+import { CommonModule, AuthGuard } from '@app/common';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [RedisModule, PrismaModule, EmailModule],
+  imports: [
+    CommonModule,
+    RedisModule,
+    PrismaModule,
+    EmailModule,
+    JwtModule.registerAsync({
+      global: true,
+      useFactory() {
+        return {
+          secret: 'Cyan',
+          signOptions: {
+            expiresIn: '30m', // 默认 30 分钟
+          },
+        };
+      },
+    }),
+  ],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [
+    UserService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class UserModule {}
--- apps/user/src/user.service.ts
+++ apps/user/src/user.service.ts
@@ -8,7 +8,8 @@ import {
 import { PrismaService } from '@app/prisma';
 import { RedisService } from '@app/redis';
 import { RegisterUserDto } from './dto/register-user.dto';
-import { md5 } from '../../exam-system/src/utils';
+import { LoginUserDto } from './dto/login-user.dto';
+import { UpdateUserPasswordDto } from './dto/update-user-password.dto';
 
 @Injectable()
 export class UserService {
@@ -45,7 +46,7 @@ export class UserService {
       return await this.prismaService.user.create({
         data: {
           username: user.username,
-          password: md5(user.password),
+          password: user.password,
           email: user.email,
         },
         select: {
@@ -58,6 +59,60 @@ export class UserService {
     } catch (e) {
       this.logger.error(e, UserService);
       return null;
+    }
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    const foundUser = await this.prismaService.user.findUnique({
+      where: {
+        username: loginUserDto.username,
+      },
+    });
+
+    if (!foundUser) {
+      throw new HttpException('用户不存在', HttpStatus.BAD_REQUEST);
+    }
+
+    if (foundUser.password !== loginUserDto.password) {
+      throw new HttpException('密码错误', HttpStatus.BAD_REQUEST);
+    }
+
+    delete foundUser.password;
+    return foundUser;
+  }
+
+  async updatePassword(passwordDto: UpdateUserPasswordDto) {
+    const captcha = await this.redisService.get(
+      `update_password_captcha_${passwordDto.email}`,
+    );
+
+    if (!captcha) {
+      throw new HttpException('验证码已失效', HttpStatus.BAD_REQUEST);
+    }
+
+    if (passwordDto.captcha !== captcha) {
+      throw new HttpException('验证码不正确', HttpStatus.BAD_REQUEST);
+    }
+
+    const foundUser = await this.prismaService.user.findUnique({
+      where: {
+        username: passwordDto.username,
+      },
+    });
+
+    foundUser.password = passwordDto.password;
+
+    try {
+      await this.prismaService.user.update({
+        where: {
+          id: foundUser.id,
+        },
+        data: foundUser,
+      });
+      return '密码修改成功';
+    } catch (e) {
+      this.logger.error(e, UserService);
+      return '密码修改失败';
     }
   }
 }
